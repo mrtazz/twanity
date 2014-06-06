6a1bdddca5c4c179ee7c4de15c3816a9a2679932
Daniel Schauenberg <d@unwiredcouch.com>
2014-06-05 22:46:34 -0400

    go fmt

--- main.go
+++ main.go
@@ -1,39 +1,38 @@
 package main
 
 import (
-  "fmt"
-  "os"
-  "flag"
-  "time"
-  "github.com/rakyll/globalconf"
-  "github.com/ChimeraCoder/anaconda"
+	"flag"
+	"fmt"
+	"github.com/ChimeraCoder/anaconda"
+	"github.com/rakyll/globalconf"
+	"os"
+	"time"
 )
 
 func main() {
 
-  var (
-    consumer_key    = flag.String("consumerkey", "", "the app consumer key")
-    consumer_secret = flag.String("consumersecret", "", "the app consumer secret")
-    access_token    = flag.String("accesstoken", "", "app access token")
-    access_secret   = flag.String("accesstokensecret", "", "app access token secret")
-    show_help       = flag.Bool("h", false, "show this dialog")
-    graphite        = flag.Bool("graphite", false, "print stats in Graphite format")
-  )
+	var (
+		consumer_key    = flag.String("consumerkey", "", "the app consumer key")
+		consumer_secret = flag.String("consumersecret", "", "the app consumer secret")
+		access_token    = flag.String("accesstoken", "", "app access token")
+		access_secret   = flag.String("accesstokensecret", "", "app access token secret")
+		show_help       = flag.Bool("h", false, "show this dialog")
+		graphite        = flag.Bool("graphite", false, "print stats in Graphite format")
+	)
 
-  conf, _ := globalconf.New("twanity")
-  conf.ParseAll()
+	conf, _ := globalconf.New("twanity")
+	conf.ParseAll()
 
-  if *show_help {
-    flag.PrintDefaults()
-    os.Exit(1)
-  }
-
+	if *show_help {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
 
 	flag.Usage = func() {
-    fmt.Fprintf(os.Stderr, "usage: twanity  username\n")
-    flag.PrintDefaults()
-    os.Exit(1)
-  }
+		fmt.Fprintf(os.Stderr, "usage: twanity  username\n")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
 
 	flag.Parse()
 
@@ -43,28 +42,28 @@ func main() {
 		flag.Usage()
 	}
 
-  anaconda.SetConsumerKey(*consumer_key)
-  anaconda.SetConsumerSecret(*consumer_secret)
-  api := anaconda.NewTwitterApi(*access_token, *access_secret)
+	anaconda.SetConsumerKey(*consumer_key)
+	anaconda.SetConsumerSecret(*consumer_secret)
+	api := anaconda.NewTwitterApi(*access_token, *access_secret)
 
-  result, err := api.GetUsersShow(username, nil)
+	result, err := api.GetUsersShow(username, nil)
 
-  if err != nil {
-    fmt.Fprintf(os.Stderr, "Unable to retrieve user details\n")
-    fmt.Fprintf(os.Stderr, "%v\n", err)
-    os.Exit(1)
-  }
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to retrieve user details\n")
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
 
-  if *graphite {
-    timestamp := time.Now().Unix()
-    fmt.Printf("twanity.%s.following %d %d\n", username, result.FriendsCount, timestamp)
-    fmt.Printf("twanity.%s.followers %d %d\n", username, result.FollowersCount, timestamp)
-    fmt.Printf("twanity.%s.tweets %d %d\n", username, result.StatusesCount, timestamp)
+	if *graphite {
+		timestamp := time.Now().Unix()
+		fmt.Printf("twanity.%s.following %d %d\n", username, result.FriendsCount, timestamp)
+		fmt.Printf("twanity.%s.followers %d %d\n", username, result.FollowersCount, timestamp)
+		fmt.Printf("twanity.%s.tweets %d %d\n", username, result.StatusesCount, timestamp)
 
-  } else {
-    fmt.Printf("Getting twitter stats for %s...\n", username)
-    fmt.Printf("Following: %d\n", result.FriendsCount)
-    fmt.Printf("Followers: %d\n", result.FollowersCount)
-    fmt.Printf("Tweets: %d\n", result.StatusesCount)
-  }
+	} else {
+		fmt.Printf("Getting twitter stats for %s...\n", username)
+		fmt.Printf("Following: %d\n", result.FriendsCount)
+		fmt.Printf("Followers: %d\n", result.FollowersCount)
+		fmt.Printf("Tweets: %d\n", result.StatusesCount)
+	}
 }
